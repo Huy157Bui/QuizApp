494e3a5c590a94e52ff3f5f0eaf0f2a36f3508d8
Huy157Bui <hoanghuy2k57@gmail.com>
2025-06-20 09:41:53 +0700

    init project

---
+++ QuizApp/src/main/java/com/bnhh/quizapp/PrimaryController.java
@@ -0,0 +1,13 @@
+package com.bnhh.quizapp;
+
+import com.bnhh.utils.MyAlert;
+import javafx.event.ActionEvent;
+
+public class PrimaryController {
+    public void handleQuestManagement(ActionEvent event){
+        MyAlert.getInstance().showMsg("Coming soon....");
+    }
+    public void handlePractice(ActionEvent event){
+        MyAlert.getInstance().showMsg("Coming soon....");
+    }
+}
